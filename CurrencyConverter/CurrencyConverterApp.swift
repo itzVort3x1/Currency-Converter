89e7397cf6568f5779fffa01278d671c42f60f0b
Sai Kaustubh <saikaustubh10@gmail.com>
2025-09-24 22:04:24 +0530

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by kaustubh on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
