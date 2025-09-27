9714f7d6a62580ddc1b081f48cdedf85f23119cd
Sai Kaustubh <saikaustubh10@gmail.com>
2025-09-27 14:44:09 +0530

    Finished app

--- CurrencyConverter/ContentView.swift
+++ CurrencyConverter/ContentView.swift
@@ -11,8 +11,12 @@ struct ContentView: View {
     
     @State var showExchangeInfo = false;
     @State var showExchangeCurrency = false;
+    
     @State var leftAmount = "";
     @State var rightAmount = "";
+    
+    @FocusState var leftTyping
+    @FocusState var rightTyping
     
     @State var leftCurrency: Currency = .silverPiece
     @State var rightCurrency: Currency = .goldPiece
@@ -44,7 +48,7 @@ struct ContentView: View {
                         }
                         
                         // text field
-                        TextField("Amount", text: $leftAmount).textFieldStyle(.roundedBorder);
+                        TextField("Amount", text: $leftAmount).textFieldStyle(.roundedBorder).focused($leftTyping)
                     }
                     // Equal sign
                     Image(systemName: "equal").font(.largeTitle).foregroundStyle(.white).symbolEffect(.pulse)
@@ -63,9 +67,9 @@ struct ContentView: View {
                         }
                         
                         // Text field
-                        TextField("Amount", text: $rightAmount).textFieldStyle(.roundedBorder).multilineTextAlignment(.trailing)
+                        TextField("Amount", text: $rightAmount).textFieldStyle(.roundedBorder).multilineTextAlignment(.trailing).focused($rightTyping)
                     }
-                }.padding().background(.black.opacity(0.5)).clipShape(.capsule)
+                }.padding().background(.black.opacity(0.5)).clipShape(.capsule).keyboardType(.decimalPad)
                 
                 Spacer()
                 
@@ -83,6 +87,22 @@ struct ContentView: View {
                     }
                 }
             }
+        }
+        .onChange(of: rightAmount) {
+            if rightTyping {
+                leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+            }
+        }
+        .onChange(of: leftAmount) {
+            if leftTyping {
+                rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+            }
+        }
+        .onChange(of: leftCurrency) {
+            leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+        }
+        .onChange(of: rightCurrency) {
+            rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
         }
     }
 }
